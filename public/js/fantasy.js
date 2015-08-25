360b8e639ca17812900f7d50edbf0d9ca2c262c3
David Sheffield <david.sheffield21@gmail.com>
2015-08-25 12:05:00 -0700

    added module for fantasy and food, updated Amazon

--- public/js/fantasy.js
+++ public/js/fantasy.js
@@ -1,8 +1,8 @@
 var pform = require('./prankforms.js');
 
 $(function(){
-  var amznModule = function(){
-      var createAmzn = function(e){
+  var fantasyModule = function(){
+      var createFantasy = function(e){
         var name = document.getElementById('#name').value,
         var email = document.getElementById('#email').value,
         var item = document.getElementById('#item').value,
@@ -12,15 +12,15 @@ $(function(){
         var zip = document.getElementById('#zip').value,
         console.log('taking in form data ' + name + ' ' + email)
       };
-      var renderAmzn = function(){
+      var renderFantasy = function(){
         $('#middle').text(name + ' ' + email + 'Your Amazon.com order of  ' + item + ' has shipped!');
         $('#middle2').text('Hello ' + name + 'Order #002-0025687-7655223 will be shipped to ' + address + city + ', ' + state + ' ' + zip)
         $('#middle3').text('Return or replace your items in Your Orders.')
         console.log('writing the middle section');
       };
-      $('#btn-click').on('click', createAmzn);
-      $('#btn-click').on('click', renderAmzn)
+      $('#btn-click').on('click', createFantasy);
+      $('#btn-click').on('click', renderFantasy)
   };
 })();
 
-exports.amznModule = amznModule;
+exports.fantasyModule = fantasyModule;
