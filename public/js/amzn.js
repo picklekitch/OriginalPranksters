8265184c893a1f93a763d6f41ee348c78bf4b108
David Sheffield <david.sheffield21@gmail.com>
2015-08-25 15:22:38 -0700

    updating amzn.js for mvp

--- public/js/amzn.js
+++ public/js/amzn.js
@@ -1,7 +1,8 @@
-var pform = require('./prankforms.js');
+// var pform = require('./prankforms.js');
 
 $(function(){
-  var amznModule = function(){
+  var Amazon = function(){
+    $('#btn-click').on('click', createAmzn(e), calcAmznCost(e), renderAmzn(e));
       var createAmzn = function(e){
         var name = document.getElementById('#name').value;
         var email = document.getElementById('#email').value;
@@ -12,15 +13,21 @@ $(function(){
         var zip = document.getElementById('#zip').value;
         console.log('taking in form data ' + name + ' ' + email);
       };
+
+      var calcAmznCost = function(e){
+        // $('#middle').text(name + ' ' + email + 'Your Amazon.com order of  ' + item + ' has shipped!');
+        // $('#middle2').text('Hello ' + name + 'Order #002-0025687-7655223 will be shipped to ' + address + city + ', ' + state + ' ' + zip);
+        // $('#middle3').text('Return or replace your items in Your Orders.');
+        console.log('writing the middle section');
+      };
+
       var renderAmzn = function(e){
         $('#middle').text(name + ' ' + email + 'Your Amazon.com order of  ' + item + ' has shipped!');
         $('#middle2').text('Hello ' + name + 'Order #002-0025687-7655223 will be shipped to ' + address + city + ', ' + state + ' ' + zip);
         $('#middle3').text('Return or replace your items in Your Orders.');
         console.log('writing the middle section');
       };
-      $('#btn-click').on('click', createAmzn);
-      $('#btn-click').on('click', renderAmzn);
   };
-})();
+}());
 
-exports.amznModule = amznModule;
+module.exports.Amazon();
