9a75880169648467be0ff6af8eb34125877368f1
David Sheffield <david.sheffield21@gmail.com>
2015-08-25 11:12:15 -0700

    add amazon module and require js to app.js

---
+++ public/js/app.js
@@ -0,0 +1,6 @@
+// Requiring our three prank methods for fantasy, amazon and food order
+var pform = require('./prankforms.js');
+var fantasy = require('./fantasy.js');
+var amzn = require('./amzn.js');
+var food = require('./food.js');
+
