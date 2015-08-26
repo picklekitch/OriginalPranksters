ce92a21d367943f856e9c43c8c3aa4a393ca0459
David Sheffield <david.sheffield21@gmail.com>
2015-08-25 17:17:46 -0700

    updating amazon js and html for amazon prank

--- public/js/amzn.js
+++ public/js/amzn.js
@@ -1,33 +1,36 @@
 // var pform = require('./prankforms.js');
 
-$(function(){
-  var Amazon = function(){
-    $('#btn-click').on('click', createAmzn(e), calcAmznCost(e), renderAmzn(e));
-      var createAmzn = function(e){
-        var name = document.getElementById('#name').value;
-        var email = document.getElementById('#email').value;
-        var item = document.getElementById('#item').value;
-        var address = document.getElementById('#address').value;
-        var city = document.getElementById('#city').value;
-        var state = document.getElementById('#state').value;
-        var zip = document.getElementById('#zip').value;
-        console.log('taking in form data ' + name + ' ' + email);
-      };
-
-      var calcAmznCost = function(e){
-        // $('#middle').text(name + ' ' + email + 'Your Amazon.com order of  ' + item + ' has shipped!');
-        // $('#middle2').text('Hello ' + name + 'Order #002-0025687-7655223 will be shipped to ' + address + city + ', ' + state + ' ' + zip);
-        // $('#middle3').text('Return or replace your items in Your Orders.');
-        console.log('writing the middle section');
-      };
-
-      var renderAmzn = function(e){
-        $('#middle').text(name + ' ' + email + 'Your Amazon.com order of  ' + item + ' has shipped!');
-        $('#middle2').text('Hello ' + name + 'Order #002-0025687-7655223 will be shipped to ' + address + city + ', ' + state + ' ' + zip);
-        $('#middle3').text('Return or replace your items in Your Orders.');
-        console.log('writing the middle section');
-      };
+var createAmzn = function(e){
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var item = $('#item option:selected').val();
+    console.log(item);
+    var howMany = $('#itemHowMany').val();
+    console.log(howMany);
+    var address = document.getElementById('address').value;
+    var city = document.getElementById('city').value;
+    var state = document.getElementById('state').value;
+    var zip = document.getElementById('zip').value;
+    console.log('taking in form data ' + name + ' ' + email);
+    return {i: item, q: howMany};
   };
-}());
 
-module.exports.Amazon();
+var calcAmznCost = function(e){
+    var total = parseInt((e.i * e.q) * 1.085);
+    console.log('calculating a number');
+    console.log(total);
+  };
+
+var renderAmzn = function(e){
+    $('#middle').text(createAmzn.name + ' ' + createAmzn.email + 'Your Amazon.com order of  ' + createAmzn.howMany + createAmzn.item + ' has shipped!');
+    $('#middle2').text('Hello ' + createAmzn.name + 'Order #002-0025687-7655223 will be shipped to ' + createAmzn.address + createAmzn.city + ', ' + createAmzn.state + ' ' + createAmzn.zip);
+    $('#middle3').text('Return or replace your items in Your Orders.');
+    console.log('sending Amazon ship notice');
+  };
+
+$('amznprank').on('click', function(e){
+    var x = createAmzn();
+    calcAmznCost(x);
+    renderAmzn();
+});
+
